d5f8f47cd5c999ee75741096086778b923fbd335
akafiver <544084263@qq.com>
2018-09-06 00:48:10 +0800

    12: 47
    运行无误

---
+++ CHAT/Controller/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  CHAT
+//
+//  Created by 文戊 on 2018/9/6.
+//  Copyright © 2018年 黑泡唱片. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
