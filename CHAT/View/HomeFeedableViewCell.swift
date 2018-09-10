ebb83327ff3586b7156bcc7a922a463b21bc9e29
akafiver <544084263@qq.com>
2018-09-10 23:14:22 +0800

    defaule-postCell设计完毕，运行无误1.1

---
+++ CHAT/View/HomeFeedableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeFeedableViewCell.swift
+//  CHAT
+//
+//  Created by 文戊 on 2018/9/10.
+//  Copyright © 2018年 黑泡唱片. All rights reserved.
+//
+
+import UIKit
+
+class HomeFeedableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
